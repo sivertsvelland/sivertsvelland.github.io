d8c05a3463f39311f570f1e348dce0def8bf1244
sivven2000 <sivert.svelland@gmail.com>
2018-11-21 18:44:11 +0100

    index

---
+++ index.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+function setup() {
+    setTimeout(openMenu, 5 * 1000);
+
+    function openMenu() {
+        window.open("file:///Users/sivertsvelland/Documents/sivertsvelland.github.io/menu/menu.html","_self")
+    }
+}
