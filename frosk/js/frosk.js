d0d9e69279af87c154e442323fbedaf1a4d20aaf
sivven2000 <sivert.svelland@gmail.com>
2018-12-14 13:06:34 +0100

    Quiz svar
    Mer utfyllende svar til quizen. Christian likte den siste best, men hans mening har ingenting å si.

--- frosk/js/frosk.js
+++ frosk/js/frosk.js
@@ -9,21 +9,19 @@ function setup() {
 
     let svar = document.getElementById("sesvar");
     svar.addEventListener("click", seSvar);
+    
 
-
-    function seSvar() {
+    function seSvar(){
         document.getElementById("spm").style.display = "none";
-        var randomWords = ["Ja", "Nei", "Kanskje"];
-
+        var randomWords = ["Ja, du vil nok sansynligvis inngå ett tvangsekteskap fordi at ingen noen gang kommer til å elske deg!", "Nei, du kommer ikke til å inngå ett tvangsekteskap for selv om du tvang noen på livet til å gifte seg med deg så ville de heller ha hoppet forann en buss! Ditt misfoster.", "Kanskje, vet da faen jeg! Trodde du virkelig at en dum quiz på en froskenettside kan svare deg på det?"];
+    
         var randomIndex = Math.floor(Math.random() * 3);//creates random No. from 1 - 3
-
+    
         document.getElementById("melding").innerHTML = randomWords[randomIndex];
         //the problem
     }
 
-    let alder = document.getElementById("alder");
-    let lagre = document.getElementById("lagreAlder");
-    lagre.addEventListener("click", lagreAlder);
+    
 
     let mann = document.getElementById("question-1-answers-A");
     mann.addEventListener("click", visSpm2);
@@ -31,9 +29,23 @@ function setup() {
     function visSpm2() {
         document.getElementById("spm2").style.display = "block";
     }
+    
+    let alder = document.getElementById("alder");
+    let lagre = document.getElementById("lagreAlder");
+    lagre.addEventListener("click", lagreAlder);
+
+    alder.addEventListener("keyup", function(event) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+          // Trigger the button element with a click
+          document.getElementById("lagreAlder").click();
+        }
+      });
 
     function lagreAlder() {
-        if (alder.value > 10) {
+        if (alder.value > 10 ){
             document.getElementById("sjekk").style.display = "none";
             document.getElementById("spm").style.display = "block";
             var source = "../media/fortnite.mp3"
@@ -42,14 +54,14 @@ function setup() {
             audio.autoplay = true;
             //
             audio.load()
-            audio.addEventListener("load", function () {
-                audio.play();
+            audio.addEventListener("load", function() { 
+                audio.play(); 
             }, true);
             audio.src = source;
         }
     }
-
-
+    
+    
 
     let question1B = document.getElementById("question-1-answers-B");
     question1B.addEventListener("click", goAway);
