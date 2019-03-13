a2287abe0478c42279e0673e7047075a9e529731
sivven2000 <sivert.svelland@gmail.com>
2019-03-13 18:37:20 +0100

    Overlap enabled
    Your enemy will now die if you shoot him.

--- warriors_2/warriors.js
+++ warriors_2/warriors.js
@@ -3,24 +3,29 @@
 function setup() {
 
     let btnStart = document.querySelector("button");
+    let btnOmstart = document.getElementById("omstart");
 
     btnStart.addEventListener("click", start);
+    btnOmstart.addEventListener("click", omstart);
 
     function start() {
         document.getElementById("board").style.display = "block";
         document.getElementById("intro").style.display = "none";
     }
 
+    function omstart() {
+        location.reload();
+    }
+
     const downKeys = new Set();
 
     let x = 55; // Kordinater for rød figur
-    let y = 55; // Kordinater for rød figur
+    let y = 55; // Kordinater for rød figur og kule
     let bullet; // Kulen til rød figur
     let bx = x; // X-akse til rød figurs kule
-    let by = y; // Y-akse for kule til bruk i overlap
 
-    let x2 = 55; // Kordinater for blå figur
-    let y2 = 55; // Kordinater for blå figur
+    let x2 = 695; // Kordinater for blå figur
+    let y2 = 495; // Kordinater for blå figur
     let bullet2; //kulen til blå figur
     let bx2 = x2; // X-akse til blå figurs kule
 
@@ -28,6 +33,7 @@ function setup() {
     let warrior = document.getElementById("warrior"); // Rød figur
     let warrior2 = document.getElementById("warrior2"); // Blå figur
     let divBoard = document.getElementById("board"); // Brett
+    let divWinner = document.getElementById("winner"); // Viser vinner
 
     document.addEventListener('keydown', event => downKeys.add(event.key)); // Trigger bevegelse
     document.addEventListener('keyup', event => downKeys.delete(event.key)); // Stopper bevegelsen
@@ -65,28 +71,28 @@ function setup() {
         // Flytter kule, fjerner kule når den når enden av brettet, for rød
         function moveBullet() {
             if (bullet) {
+                bullet.style.top = y;
                 bullet.style.left = bx++ + "px";
-                // Overlapp test er kommentert ut fordi at den hindret figuren i å skyte
-                // det ble registrert overlapp med en gang bullet ble laget.
-                /*
-                if (overlap({ x: x2, y: y2, w: 50, h: 75 }, { x: bx, y: by, w: 20, h: 20 })) {
-                    alert("treff");
+
+                if (overlap({ x: x2, y: y2, w: 50, h: 75 }, { x: bx, y: y, w: 20, h: 20 })) {
+                    document.getElementById("warrior2").style.display = "none";
+                    document.getElementById("winner").style.display = "block";
+                    divWinner.innerHTML = "Red warrior won!";
+                    document.getElementById("omstart").style.display = "block";
                 }
-                */
+
                 if (bx > 780) {
                     divBoard.removeChild(bullet);
                     return;
                 }
                 window.requestAnimationFrame(moveBullet);
             }
-            /*
-            function overlap(a, b) {
-                return (a.x > b.x - a.w && a.x < b.x + b.w && a.y > b.y - a.h && a.y < b.y + b.h);
-             }
-             */
-        }
 
-        
+            function overlap(a, b) {
+                return (a.x < b.x - a.w && a.y < b.y + a.w && a.y > b.y - a.h && b.y < a.y + b.h);
+            }
+
+        }
 
         window.requestAnimationFrame(moveWarrior);
         window.requestAnimationFrame(moveBullet);
@@ -97,10 +103,10 @@ function setup() {
         function isDown(key) { return downKeys.has(key) }
         const s2 = warrior2.style;
         const a2 = isDown('-') ? 10 : 1; // Boost
-        if (isDown('ArrowDown')) s2.bottom = (y2 -= a2) + 'px'; if (y2 <= 3) { y2 = 3; }
-        if (isDown('ArrowUp')) s2.bottom = (y2 += a2) + 'px'; if (y2 >= 530) { y2 = 530; }
-        if (isDown('ArrowLeft')) s2.right = (x2 += a2) + 'px'; if (x2 >= 731) { x2 = 731; }
-        if (isDown('ArrowRight')) s2.right = (x2 -= a2) + 'px'; if (x2 <= 17) { x2 = 17; }
+        if (isDown('ArrowDown')) s2.top = (y2 += a2) + 'px'; if (y2 <= 3) { y2 = 3; }
+        if (isDown('ArrowUp')) s2.top = (y2 -= a2) + 'px'; if (y2 >= 530) { y2 = 530; }
+        if (isDown('ArrowLeft')) s2.left = (x2 -= a2) + 'px'; if (x2 >= 731) { x2 = 731; }
+        if (isDown('ArrowRight')) s2.left = (x2 += a2) + 'px'; if (x2 <= 17) { x2 = 17; }
 
         document.addEventListener("keydown", shoot2);
         //trenger ikke setInterval i denne koden
@@ -112,8 +118,8 @@ function setup() {
                     if (!bullet2) {
                         bullet2 = document.createElement("div");
                         bullet2.className = "bullet2";
-                        bullet2.style.bottom = y2 + 'px';
-                        bullet2.style.right = x2 + 'px';
+                        bullet2.style.top = y2 + 'px';
+                        bullet2.style.left = x2 + 'px';
                         divBoard.appendChild(bullet2);
                         moveBullet2();
 
@@ -123,11 +129,23 @@ function setup() {
         }
 
         // Flytter kule, fjerner kule når den når enden av brettet, for blå
-        
-            function moveBullet2() {
-                if (bullet2) {
-                bullet2.style.right = bx2++ + "px";
-                if (bx2 > 780) {
+
+        function moveBullet2() {
+            if (bullet2) {
+                bullet2.style.left = bx2-- + "px";
+
+                if (overlap2({ x: x, y: y, w: 50, h: 75 }, { x: bx2, y: y2, w: 20, h: 20 })) {
+                    document.getElementById("warrior").style.display = "none";
+                    document.getElementById("winner").style.display = "block";
+                    divWinner.innerHTML = "Blue warrior won!";
+                    document.getElementById("omstart").style.display = "block";
+                }
+
+                function overlap2(a, b) {
+                    return (a.x > b.x && a.y < b.y + a.w && a.y > b.y - a.h && b.y < a.y + b.h);
+                }
+
+                if (bx2 < 0) {
                     divBoard.removeChild(bullet2);
                     return;
                 }
